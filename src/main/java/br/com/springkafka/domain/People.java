fa7ce75ca30dea0d1f52d1c4a6c327a7005d64f2
Bruno Praciano <bjpraciano@gmail.com>
2022-02-16 15:38:44 +0100

    Create persistency for kafka topic

--- src/main/java/br/com/springkafka/domain/People.java
+++ src/main/java/br/com/springkafka/domain/People.java
@@ -1,14 +1,16 @@
 package br.com.springkafka.domain;
 
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class People {
 
@@ -19,7 +21,7 @@ public class People {
     private String name;
     private String cpf;
 
-    @OneToMany(mappedBy = "people")
+    @OneToMany(mappedBy = "people", cascade = CascadeType.ALL)
     private List<Book> books;
 
 
